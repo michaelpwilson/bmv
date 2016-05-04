ea64c76a086dc1841da2638297f3630fdd1ee254
Michael Wilson <michael.wilson@greenlightdigital.com>
2016-05-04 14:22:32 +0100

    beautified the code

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ const mv = require('mv');
 const async = require("async");
 
 var mvBack = {
-  options: {},
-  init: function(opts) {
-    // if there are options
-    // add them to the options property
-    if(opts) this.options = opts;
-    return this;
-  },
-  move: function(batch) {
+    options: {},
+    init: function(opts) {
+        // if there are options
+        // add them to the options property
+        if (opts) this.options = opts;
+        return this;
+    },
+    move: function(batch) {
 
-    async.each(batch, function(files, callback){
+        async.each(batch, function(files, callback) {
 
-      mv(files[0], files[1], function(err) {
-        if(err) callback(err);
+            mv(files[0], files[1], function(err) {
+                if (err) callback(err);
 
-        callback();
-      });
+                callback();
+            });
 
-    }, function(err){
+        }, function(err) {
 
-      if(err) throw new Error(err);
-      console.log("files have been moved");
+            if (err) throw new Error(err);
+            console.log("files have been moved");
 
-    });
+        });
 
-  }
+    }
 }
 
 module.exports = mvBack.init();
